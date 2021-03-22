88e061d10293735289346ccc14ca0208e7872fdd
harsh jain <harshjniitr@gmail.com>
2021-03-22 12:12:34 +0530

    change contract

--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ import "assets/styles/tailwind.css";
 
 // layouts
 
-import Admin from "layouts/Admin.js";
-import Auth from "layouts/Auth.js";
+// import Admin from "layouts/Admin.js";
+// import Auth from "layouts/Auth.js";
 
 // views without layouts
 
-import Landing from "views/Landing.js";
+// import Landing from "views/Landing.js";
 import Profile from "views/Profile.js";
 import Index from "views/Index.js";
 import FAQ from "views/Faq.js";
@@ -26,10 +26,10 @@ ReactDOM.render(
     <HashRouter>
       <Switch>
         {/* add routes with layouts */}
-        <Route path="/admin" component={Admin} />
-        <Route path="/auth" component={Auth} />
+        {/* <Route path="/admin" component={Admin} />
+        <Route path="/auth" component={Auth} /> */}
         {/* add routes without layouts */}
-        <Route path="/landing" exact component={Landing} />
+        {/* <Route path="/landing" exact component={Landing} /> */}
         <Route path="/profile" exact component={Profile} />
         <Route path="/" exact component={Index} />
         <Route path="/faq" exact component={FAQ} />
