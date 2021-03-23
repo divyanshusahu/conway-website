70d9e1b29492b5ccf2a6d83dad9d0abeb253c4c2
harsh jain <harshjniitr@gmail.com>
2021-03-23 22:12:27 +0530

    Add

--- src/blockchain/constant.js
+++ src/blockchain/constant.js
@@ -1,6 +1,6 @@
-export const GOL_ADDRESS = "0x1f4C5782F2D575aD7BF0EaCe3577e5b6BB79F727";
-export const GolNFT = "GOL";
-export const TWITTER_HANDLE = "@fibi";
+export const GOL_ADDRESS = "0xB91c21aF0328782a9ad9013646FC150E409076B3";
+export const GolNFT = "mysticGOL";
+export const TWITTER_HANDLE = "_mysticGol";
 export const CHAIN_ID = 3; //97; // 56 for bsc, 97 for testnet
-export const TELEGRAM_URL = "https://t.me"; //97; // 56 for bsc, 97 for testnet
+export const TELEGRAM_URL = "https://t.me/mysticGol"; //97; // 56 for bsc, 97 for testnet
 export const DEFAULT_ADDR = "0x0000000000000000000000000000000000000000"; //97; // 56 for bsc, 97 for testnet
